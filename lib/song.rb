23974a411753a26fefc9b4860874336f6acca8b3
fadimata tillman <fadi.tillman@gmail.com>
2020-02-13 23:07:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
 class Song
-  attr_accessor :artist, :name
+   attr_accessor :name, :artist
 
-  @@all = []
+   @@all = []
 
-  def initialize(name)
-    @name = name
-    save
-  end
+   def initialize(name)
+     @name = name
+     @@all << self
+   end
 
-  def save
-    @@all << self
-  end
+   def self.all
+     @@all
+   end
 
-  def self.all
-    @@all
-  end
-end
+   def artist_name
+     artist.name if artist
+   end
+ end
