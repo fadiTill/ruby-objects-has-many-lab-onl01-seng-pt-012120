c73535d2e3476d6e88ef2a128625d8afcb8f9187
fadimata tillman <fadi.tillman@gmail.com>
2020-02-13 22:35:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,22 @@
 
+# class Artist
+#   attr_accessor :name
+#   def initialize (name)
+#     @name = name
+# end
+#
+# def songs
+#   @songs = song
+# end
+# end
 class Artist
   attr_accessor :name
-  def initialize (name)
-    @name = name
-end
 
-def songs
-  @songs = song
-end
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
